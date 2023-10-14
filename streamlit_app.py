34c7865a6b44f3bb48e668814ad964b3dc1eb4f4
Barbara Rzepka <bbrzepka@gmail.com>
2023-10-14 19:17:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,10 @@ st.dataframe(fruits_to_show)
 
 st.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+
+# normalize json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show data as a table
+streamlit.dataframe(fruityvice_normalized)
+
+
