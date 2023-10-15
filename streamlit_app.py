b448f6e5935659655433e46070daa276e977d516
Barbara Rzepka <bbrzepka@gmail.com>
2023-10-16 00:00:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ st.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_rows = my_cur.fetchone()
+st.header("Fruit list:")
+st.dataframe(my_data_rows)
 
