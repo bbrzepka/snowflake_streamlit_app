14d965df8eea7279d027e385b6c8a4892250a790
Barbara Rzepka <bbrzepka@gmail.com>
2023-10-16 20:21:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,7 @@ if streamlit.button('Get Fruit Load List'):
   st.header("Fruit list:")
   st.dataframe(my_data_rows)
   
-  try:
-    fruit_add = st.text_input('What fruit would you like to add?')
-    st.write('Thanks for adding: ', fruit_add)
+fruit_add = st.text_input('What fruit would you like to add?')
+st.write('Thanks for adding: ', fruit_add)
+if fruit_add not in my_data_rows:
+  my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + fruit_add + "');")
