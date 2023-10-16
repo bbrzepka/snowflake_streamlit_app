bbedafd6a94b279bda3aad42b9c7893004664adf
Barbara Rzepka <bbrzepka@gmail.com>
2023-10-16 19:58:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,17 +36,20 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # show data as a table
 st.dataframe(fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-st.header("Fruit list:")
-st.dataframe(my_data_rows)
-
-
-try:
-  fruit_add = st.text_input('What fruit would you like to add?')
-  st.write('Thanks for adding: ', fruit_add)
-if fruit_add not in my_data_rows:
-  my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + fruit_add + "');")
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  my_cur.execute("SELECT * FROM fruit_load_list")
+  my_data_rows = my_cur.fetchall()
+  st.header("Fruit list:")
+  st.dataframe(my_data_rows)
+  
+  
+  try:
+    fruit_add = st.text_input('What fruit would you like to add?')
+    st.write('Thanks for adding: ', fruit_add)
+    print(type(my_data_rows))
+  if fruit_add not in my_data_rows:
+    my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + fruit_add + "');")
 
